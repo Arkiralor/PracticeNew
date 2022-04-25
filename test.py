911272b15d94c9b0bb6ab5ab56ade8dc73a742ba
Arkiralor <pritho11335@gmail.com>
2022-04-25 16:12:38 +0530

    test

---
+++ test.py
@@ -0,0 +1,11 @@
+'''
+Test script for demo push to git
+'''
+
+from secrets import token_hex
+
+def test_token_hex(num:int):
+    return token_hex(num)
+
+if __name__=="__main__":
+    print(test_token_hex(256))
